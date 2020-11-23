376a5dbf44be7de10e0de04f58775163abb23656
nicolasconde <nicolas.conde@bandtec.com.br>
2020-11-22 21:53:54 -0300

    Versão 2.2 - Página fanArt

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// SCROLL
 function scrollSinopse() {
     var scroll = document.getElementById("sinopse");
     scroll.scrollIntoView();
@@ -20,3 +21,279 @@ function scrollSinopse() {
     var scroll = document.getElementById("sinopse");
     scroll.scrollIntoView();
 }
+// FIM SCROLL
+// MODAL1
+const botaoAbrir1 = document.querySelector("#abrir1");
+const botaoFechar1 = document.querySelector('#fechar1');
+const containerModal1 = document.querySelector("#container1");
+
+function abrirModal1() {
+    event.preventDefault();
+    this
+    containerModal1.classList.add('ativo');
+}
+function fecharModal1(event) {
+    event.preventDefault();
+    containerModal1.classList.remove('ativo');
+}
+function cliqueForaModal1(event) {
+    if (event.target === this) {
+        fecharModal1(event);
+    }
+}
+botaoAbrir1.addEventListener('click', abrirModal1);
+botaoFechar1.addEventListener('click', fecharModal1);
+containerModal1.addEventListener('click', cliqueForaModal1);
+
+// MODAL2
+const botaoAbrir2 = document.querySelector("#abrir2");
+const botaoFechar2 = document.querySelector('#fechar2');
+const containerModal2 = document.querySelector("#container2");
+
+function abrirModal2() {
+    event.preventDefault();
+    this
+    containerModal2.classList.add('ativo');
+}
+function fecharModal2(event) {
+    event.preventDefault();
+    containerModal2.classList.remove('ativo');
+}
+function cliqueForaModal2(event) {
+    if (event.target === this) {
+        fecharModal2(event);
+    }
+}
+botaoAbrir2.addEventListener('click', abrirModal2);
+botaoFechar2.addEventListener('click', fecharModal2);
+containerModal2.addEventListener('click', cliqueForaModal2);
+
+// MODAL3
+const botaoAbrir3 = document.querySelector("#abrir3");
+const botaoFechar3 = document.querySelector('#fechar3');
+const containerModal3 = document.querySelector("#container3");
+
+function abrirModal3() {
+    event.preventDefault();
+    this
+    containerModal3.classList.add('ativo');
+}
+function fecharModal3(event) {
+    event.preventDefault();
+    containerModal3.classList.remove('ativo');
+}
+function cliqueForaModal3(event) {
+    if (event.target === this) {
+        fecharModal3(event);
+    }
+}
+botaoAbrir3.addEventListener('click', abrirModal3);
+botaoFechar3.addEventListener('click', fecharModal3);
+containerModal3.addEventListener('click', cliqueForaModal3);
+
+// MODAL4
+const botaoAbrir4 = document.querySelector("#abrir4");
+const botaoFechar4 = document.querySelector('#fechar4');
+const containerModal4 = document.querySelector("#container4");
+
+function abrirModal4() {
+    event.preventDefault();
+    this
+    containerModal4.classList.add('ativo');
+}
+function fecharModal4(event) {
+    event.preventDefault();
+    containerModal4.classList.remove('ativo');
+}
+function cliqueForaModal4(event) {
+    if (event.target === this) {
+        fecharModal4(event);
+    }
+}
+botaoAbrir4.addEventListener('click', abrirModal4);
+botaoFechar4.addEventListener('click', fecharModal4);
+containerModal4.addEventListener('click', cliqueForaModal4);
+
+// MODAL5
+const botaoAbrir5 = document.querySelector("#abrir5");
+const botaoFechar5 = document.querySelector('#fechar5');
+const containerModal5 = document.querySelector("#container5");
+
+function abrirModal5() {
+    event.preventDefault();
+    this
+    containerModal5.classList.add('ativo');
+}
+function fecharModal5(event) {
+    event.preventDefault();
+    containerModal5.classList.remove('ativo');
+}
+function cliqueForaModal5(event) {
+    if (event.target === this) {
+        fecharModal5(event);
+    }
+}
+botaoAbrir5.addEventListener('click', abrirModal5);
+botaoFechar5.addEventListener('click', fecharModal5);
+containerModal5.addEventListener('click', cliqueForaModal5);
+
+// MODAL6
+const botaoAbrir6 = document.querySelector("#abrir6");
+const botaoFechar6 = document.querySelector('#fechar6');
+const containerModal6 = document.querySelector("#container6");
+
+function abrirModal6() {
+    event.preventDefault();
+    this
+    containerModal6.classList.add('ativo');
+}
+function fecharModal6(event) {
+    event.preventDefault();
+    containerModal6.classList.remove('ativo');
+}
+function cliqueForaModal6(event) {
+    if (event.target === this) {
+        fecharModal6(event);
+    }
+}
+botaoAbrir6.addEventListener('click', abrirModal6);
+botaoFechar6.addEventListener('click', fecharModal6);
+containerModal6.addEventListener('click', cliqueForaModal6);
+
+// MODAL7
+const botaoAbrir7 = document.querySelector("#abrir7");
+const botaoFechar7 = document.querySelector('#fechar7');
+const containerModal7 = document.querySelector("#container7");
+
+function abrirModal7() {
+    event.preventDefault();
+    this
+    containerModal7.classList.add('ativo');
+}
+function fecharModal7(event) {
+    event.preventDefault();
+    containerModal7.classList.remove('ativo');
+}
+function cliqueForaModal7(event) {
+    if (event.target === this) {
+        fecharModal7(event);
+    }
+}
+botaoAbrir7.addEventListener('click', abrirModal7);
+botaoFechar7.addEventListener('click', fecharModal7);
+containerModal7.addEventListener('click', cliqueForaModal7);
+
+// MODAL8
+const botaoAbrir8 = document.querySelector("#abrir8");
+const botaoFechar8 = document.querySelector('#fechar8');
+const containerModal8 = document.querySelector("#container8");
+
+function abrirModal8() {
+    event.preventDefault();
+    this
+    containerModal8.classList.add('ativo');
+}
+function fecharModal8(event) {
+    event.preventDefault();
+    containerModal8.classList.remove('ativo');
+}
+function cliqueForaModal8(event) {
+    if (event.target === this) {
+        fecharModal8(event);
+    }
+}
+botaoAbrir8.addEventListener('click', abrirModal8);
+botaoFechar8.addEventListener('click', fecharModal8);
+containerModal8.addEventListener('click', cliqueForaModal8);
+
+// MODAL9
+const botaoAbrir9 = document.querySelector("#abrir9");
+const botaoFechar9 = document.querySelector('#fechar9');
+const containerModal9 = document.querySelector("#container9");
+
+function abrirModal9() {
+    event.preventDefault();
+    this
+    containerModal9.classList.add('ativo');
+}
+function fecharModal9(event) {
+    event.preventDefault();
+    containerModal9.classList.remove('ativo');
+}
+function cliqueForaModal9(event) {
+    if (event.target === this) {
+        fecharModal9(event);
+    }
+}
+botaoAbrir9.addEventListener('click', abrirModal9);
+botaoFechar9.addEventListener('click', fecharModal9);
+containerModal9.addEventListener('click', cliqueForaModal9);
+
+// MODAL10
+const botaoAbrir10 = document.querySelector("#abrir10");
+const botaoFechar10 = document.querySelector('#fechar10');
+const containerModal10 = document.querySelector("#container10");
+
+function abrirModal10() {
+    event.preventDefault();
+    this
+    containerModal10.classList.add('ativo');
+}
+function fecharModal10(event) {
+    event.preventDefault();
+    containerModal10.classList.remove('ativo');
+}
+function cliqueForaModal10(event) {
+    if (event.target === this) {
+        fecharModal10(event);
+    }
+}
+botaoAbrir10.addEventListener('click', abrirModal10);
+botaoFechar10.addEventListener('click', fecharModal10);
+containerModal10.addEventListener('click', cliqueForaModal10);
+
+// MODAL11
+const botaoAbrir11 = document.querySelector("#abrir11");
+const botaoFechar11 = document.querySelector('#fechar11');
+const containerModal11 = document.querySelector("#container11");
+
+function abrirModal11() {
+    event.preventDefault();
+    this
+    containerModal11.classList.add('ativo');
+}
+function fecharModal11(event) {
+    event.preventDefault();
+    containerModal11.classList.remove('ativo');
+}
+function cliqueForaModal11(event) {
+    if (event.target === this) {
+        fecharModal11(event);
+    }
+}
+botaoAbrir11.addEventListener('click', abrirModal11);
+botaoFechar11.addEventListener('click', fecharModal11);
+containerModal11.addEventListener('click', cliqueForaModal11);
+
+// MODAL12
+const botaoAbrir12 = document.querySelector("#abrir12");
+const botaoFechar12 = document.querySelector('#fechar12');
+const containerModal12 = document.querySelector("#container12");
+
+function abrirModal12() {
+    event.preventDefault();
+    this
+    containerModal12.classList.add('ativo');
+}
+function fecharModal12(event) {
+    event.preventDefault();
+    containerModal12.classList.remove('ativo');
+}
+function cliqueForaModal12(event) {
+    if (event.target === this) {
+        fecharModal12(event);
+    }
+}
+botaoAbrir12.addEventListener('click', abrirModal12);
+botaoFechar12.addEventListener('click', fecharModal12);
+containerModal12.addEventListener('click', cliqueForaModal12);
